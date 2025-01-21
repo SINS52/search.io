f9a411b75721cea5f66f3ce3efcd1d837d0e6d5d
SINS52 <denye.syahputra10@smp.belajar.id>
2025-01-21 04:07:27 +0000

    update

--- script.js
+++ script.js
@@ -2,11 +2,28 @@ function toggleSearchButton() {
   const input = document.getElementById("src");
   const button = document.getElementById("searchButton");
 
-  // Aktifkan tombol jika input tidak kosong
+
   button.disabled = input.value.trim() === "";
 }
 
 function toggleMenu() {
   var menu = document.querySelector('nav');
-  menu.classList.toggle('active');
+  menu.classList.toggle('active');  
 }
+
+function changeThemes() {
+  var body = document.querySelector('body');
+  body.classList.toggle('active');
+  
+  var img = document.querySelector('img'); 
+  if (body.classList.contains('active')) {
+    img.src = 'Blue_Archive_EN_logo.svg.png';
+  } else {
+    img.src = 'hoyoverse-logo-42461.png';
+  } 
+}
+
+
+document.querySelector('button').addEventListener('click', changeThemes);
+
+
